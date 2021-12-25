446106ae3eb6243bab1a5703c73a2a87bcca63a2
Igor Hörlle <91513127+Ihorlle@users.noreply.github.com>
2021-12-25 16:24:00 -0300

    Add files via upload

---
+++ Ex02SobrecargaMetodos/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.text.PlainDocument;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Quadrilatero
+    System.out.println("Exercicio Quadrilatero");
+    quadrilatero.area(3);
+    quadrilatero.area(5d,5d);
+    quadrilatero.area(7, 8, 9);
+    quadrilatero.area(5f,5f);
+    }
+}
